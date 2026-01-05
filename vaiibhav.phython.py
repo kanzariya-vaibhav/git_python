ce81b09ed2929671bbe544a03b0073a77df71ccd
kanzariya-vaibhav <kanzariyavaibhav136@gmail.com>
2026-01-05 10:35:16 +0530

    Add files via upload

---
+++ vaiibhav.phython.py
@@ -0,0 +1,47 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> x=15
+>>> print (x)
+15
+>>> y=9
+>>> print (-y)
+-9
+>>> x=56
+>>> y=39
+>>> print (x+y)
+95
+>>> x=55
+>>> y=63
+>>> print (x-y)
+-8
+>>> x=15
+>>> print (x)
+15
+>>> y=40
+>>> print (y)
+40
+>>> x=100
+>>> y=150
+>>> print (x+y)
+250
+>>> x=50
+>>> y=50
+>>> print (x*y)
+2500
+>>> x=150
+>>> y=150
+>>> print (x|y)
+150
+>>> x=99
+>>> y=5
+>>> print(x/y)
+19.8
+>>> x=True
+>>> y=False
+>>> print(x)
+True
+>>> print(y)
+False
+>>> 
