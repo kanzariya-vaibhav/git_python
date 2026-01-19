e98e9611f66f6e48949a29767608f27585136763
kanzariya-vaibhav <kanzariyavaibhav136@gmail.com>
2026-01-19 10:39:57 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,39 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello")
+hello
+>>> x=15
+>>> print(x)
+15
+>>> y=9
+>>> print(-y)
+-9
+>>> x=56
+>>> y=99
+>>> print(x+y)
+155
+>>> x=77
+>>> y=39
+>>> print(x-y)
+38
+>>> x=69
+>>> y=53
+>>> print(x*y)
+3657
+>>> x=99
+>>> y=2
+>>> print(x|y)
+99
+>>> x=99
+>>> y=5
+>>> print(x/y)
+19.8
+>>> x=True
+>>> y=False
+>>> print(x)
+True
+>>> print(y)
+False
+>>> x=56
+>>> 
+>>> 
